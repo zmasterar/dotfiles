5123de51c480f7d50a07afdd8dbd8574488a98d5
Benjamin Lavena <benja@zmaster.com.ar>
2023-03-08 08:54:20 -0300

    Fixed aliases script

--- install.zsh
+++ install.zsh
@@ -1,4 +1,4 @@
-ln -s $HOME/dotfiles/zsh_custom/aliases.zsh $ZSH_CUSTOM/aliases.zsh
+ln -s $HOME/dotfiles/zsh_custom/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 ln -s $HOME/dotfiles/zsh_custom/ssh_agent.zsh $ZSH_CUSTOM/ssh_agent.zsh
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
--- zsh_custom/aliases/aliases.zsh
+++ zsh_custom/aliases/aliases.zsh
@@ -1,9 +1,10 @@
 # ln -s ~/dotfiles/zsh_custom/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 search_dir=~/dotfiles/zsh_custom/aliases
-for file in "$search_dir"/*
-do
-  if [[ "$file" = '/home/zmaster/dotfiles/zsh_custom/aliases/aliases.zsh' ]]; then
+for file in "$search_dir"/* 
+do 
+  filename=$(basename -- "$file")
+  if [[ "$filename" = "aliases.zsh" ]]; then
     continue
   fi
-  source "$file"
+  source $file
 done
