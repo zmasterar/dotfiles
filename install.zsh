69c0386d699fc60d7e3e873e9df0ddd82a091804
Benjamin Lavena <benja@zmaster.com.ar>
2023-04-05 19:33:16 -0300

    Added docker_run_local to install script

--- install.zsh
+++ install.zsh
@@ -1,5 +1,6 @@
 ln -s $HOME/dotfiles/zsh_custom/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 ln -s $HOME/dotfiles/zsh_custom/ssh_agent.zsh $ZSH_CUSTOM/ssh_agent.zsh
+ln -s $HOME/dotfiles/zsh_custom/docker_run_local.zsh $ZSH_CUSTOM/docker_run_local.zsh
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
