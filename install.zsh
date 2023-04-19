6c0b4a2e5b7826deaf40193eb45a52e899c1783d
Benjamin Lavena <benja@zmaster.com.ar>
2023-04-19 09:54:24 -0300

    Improved install.zsh

--- install.zsh
+++ install.zsh
@@ -1,6 +1,15 @@
-ln -s $HOME/dotfiles/zsh_custom/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
-ln -s $HOME/dotfiles/zsh_custom/ssh_agent.zsh $ZSH_CUSTOM/ssh_agent.zsh
-ln -s $HOME/dotfiles/zsh_custom/docker_run_local.zsh $ZSH_CUSTOM/docker_run_local.zsh
-ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+set -x
+
+# Script aliases.zsh sources all files inside aliases directory
+ln -f -s $HOME/dotfiles/zsh_custom/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
+
+# Makes a symbolic link for every file in zsh_custom directory
+ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
+for file in $(find ./zsh_custom -maxdepth 1 -type f -printf "%f\n"); do 
+  ln -f -s $HOME/dotfiles/zsh_custom/$file $ZSH_CUSTOM/$file
+done
+
+# Makes a symbolic link in for every file in base_dot_files directory
+for file in $(find ./base_dot_files -type f -printf "%f\n"); do 
+  ln -f -s $HOME/dotfiles/base_dot_files/$file $HOME/.$file
+done
