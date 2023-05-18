04b193dfb67a1315c1f5d53521896d169e759f32
Benjamin Lavena <blavena@justucuman.gov.ar>
2023-05-17 21:03:42 -0300

    Added gla (git fetch all recursively) function

--- aliases/git_aliases.zsh
+++ aliases/git_aliases.zsh
@@ -1,1 +1,7 @@
-alias gla="$HOME/pjt/git_pull_all.sh"
+gla() {
+  export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/deploy"
+
+  cd $HOME
+  find . -type d -name .git -exec bash -c "echo {} && git --git-dir={} --work-tree=$PWD/{}/.. fetch --all && echo" \;
+  cd -
+}
