2afec1635ced9501ebd71350631e44e8e309015a
Benjamin Lavena <blavena@justucuman.gov.ar>
2023-05-17 21:29:16 -0300

    Updated git pull all script

--- other_scritps/git_pull_all.sh
+++ other_scritps/git_pull_all.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-path="$HOME/pjt"
+
+# crontab
+# @reboot $HOME/git_pull_all.sh
+# 5 7 * * 1-5 $HOME/git_pull_all.sh
+
+export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/deploy"
+path="$HOME"
 
 /usr/bin/notify-send "git pull all" "Executed git pull all! \n $(date "+%d/%b/%Y %H:%M:%S")"
-echo $(date "+%Y-%m-%d %H:%M:%S") > $HOME/pjt/git_pull_all.log 2>&1
-eval `ssh-agent -s` && ssh-add "$HOME/.ssh/deploy" && ssh-add -l
-cd $path && find . -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull" \; >> $HOME/pjt/git_pull_all.log 2>&1
+echo $(date "+%Y-%m-%d %H:%M:%S") > $HOME/git_pull_all.log 2>&1
+cd $path
+find . -type d -name .git -exec bash -c "echo {} && git --git-dir={} --work-tree=$PWD/{}/.. fetch --all && echo" \; >> $HOME/git_pull_all.log 2>&1
+
