c7c323ac0bc147a6f9b5667c4a0c0ccdac705bed
Benjamin Lavena <blavena@justucuman.gov.ar>
2023-06-01 08:11:09 -0300

    updates to git pull all script using dunstify

--- other_scritps/git_pull_all.sh
+++ other_scritps/git_pull_all.sh
@@ -5,10 +5,30 @@
 # 5 7 * * 1-5 XDG_RUNTIME_DIR=/run/user/$(id -u) $HOME/git_pull_all.sh
 
 export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/deploy"
-path="$HOME"
+path="$HOME/pjt"
 
-/usr/bin/notify-send "git pull all" "Executed git pull all! \n $(date "+%d/%b/%Y %H:%M:%S")"
+handle_dismiss () {
+  true
+}
+
+open_log () {
+  code $HOME/git_pull_all.log &
+}
+
 echo $(date "+%Y-%m-%d %H:%M:%S") > $HOME/git_pull_all.log 2>&1
 cd $path
 find . -type d -name .git -exec bash -c "echo {} && git --git-dir={} --work-tree=$PWD/{}/.. fetch --all && echo" \; >> $HOME/git_pull_all.log 2>&1
+
+ACTION=$(/usr/bin/dunstify --action="default,Open Log" --action="open_log,Open Log" -h string:x-dunst-stack-tag:gpl "git pull all" "Executed git pull all! \n $(date "+%d/%b/%Y %H:%M:%S")")
+case "$ACTION" in
+"default")
+    open_log
+    ;;
+"open_log")
+    open_log
+    ;;
+"2")
+    handle_dismiss
+    ;;
+esac
 
