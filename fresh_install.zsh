eac8d24b3a0e013a4dd955c36cb655a0c0674ff5
Benjamin Lavena <benja@zmaster.com.ar>
2024-01-21 21:32:47 -0300

    Added git-flow-completion plugin install

--- fresh_install.zsh
+++ fresh_install.zsh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.ssh
 chmod  0700 $HOME/.ssh
 # For reference, private keys must have 0600 permissions
 
-sudo apt update && sudo apt install -y zsh curl tmux
+sudo apt update && sudo apt install -y zsh curl tmux wireguard
 
 sudo chsh -s $(which zsh) $(whoami)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -46,6 +46,7 @@ ln -f -s $HOME/dotfiles/other_scripts/dark_colors.yaml $HOME/.config/colorls
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/bobthecow/git-flow-completion $ZSH_CUSTOM/plugins/git-flow-completion
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
