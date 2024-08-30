8b5768c42f03db7e10076e03c77504544c9a0090
Benjamin Lavena <benja@zmaster.com.ar>
2024-08-30 13:03:56 -0300

    Fixed symlinks

--- fresh_install.zsh
+++ fresh_install.zsh
@@ -27,7 +27,7 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install kubecolor
 
 # Makes a symbolic link in for every file in base_dot_files directory
-for file in $(find ./base_dot_files -type f -printf "%f\n"); do 
+for file in $(find $HOME/dotfiles/base_dot_files -type f -printf "%f\n"); do 
   ln -f -s $HOME/dotfiles/base_dot_files/$file $HOME/.$file
 done
 
@@ -36,7 +36,7 @@ ZSH_CUSTOM=${HOME}/.oh-my-zsh/custom
 ln -f -s $HOME/dotfiles/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 
 # Makes a symbolic link for every file in zsh_custom directory
-for file in $(find ./zsh_custom -maxdepth 1 -type f -printf "%f\n"); do 
+for file in $(find $HOME/dotfiles/zsh_custom -maxdepth 1 -type f -printf "%f\n"); do 
   ln -f -s $HOME/dotfiles/zsh_custom/$file $ZSH_CUSTOM/$file
 done
 
