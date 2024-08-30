c4e519870dd5e7e3b844e1500b48ca43a5b69e47
Benjamin Lavena <benja@zmaster.com.ar>
2024-08-30 13:07:26 -0300

    Changed order

--- fresh_install.zsh
+++ fresh_install.zsh
@@ -10,7 +10,7 @@ chmod  0700 $HOME/.ssh
 mkdir -p $HOME/.local/bin
 
 sudo add-apt-repository -y ppa:mattrose/terminator
-sudo apt update && sudo apt install -y zsh git curl tmux wireguard jq vim terminator
+sudo apt update && sudo apt install -y terminator zsh git curl tmux wireguard jq vim
 
 if [ ! -d $HOME/dotfiles ]; then
   git clone https://github.com/zmasterar/dotfiles.git $HOME/dotfiles
@@ -21,10 +21,6 @@ sudo apt install -y autoconf build-essential libbz2-dev libdb-dev libffi-dev lib
 
 sudo chsh -s $(which zsh) $(whoami)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install kubecolor
 
 # Makes a symbolic link in for every file in base_dot_files directory
 for file in $(find $HOME/dotfiles/base_dot_files -type f -printf "%f\n"); do 
@@ -39,6 +35,13 @@ ln -f -s $HOME/dotfiles/aliases/aliases.zsh $ZSH_CUSTOM/aliases.zsh
 for file in $(find $HOME/dotfiles/zsh_custom -maxdepth 1 -type f -printf "%f\n"); do 
   ln -f -s $HOME/dotfiles/zsh_custom/$file $ZSH_CUSTOM/$file
 done
+
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim -es +PluginInstall +qall
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install kubecolor
 
 # mise
 curl https://mise.run | sh
@@ -71,8 +74,6 @@ git clone https://github.com/bobthecow/git-flow-completion $ZSH_CUSTOM/plugins/g
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/jimeh/tmux-themepack.git $HOME/.tmux-themepack
 
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-
 mkdir $HOME/.fonts
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
@@ -80,4 +81,3 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20I
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 mv *.ttf $HOME/.fonts
 
-vim -es +PluginInstall +qall
