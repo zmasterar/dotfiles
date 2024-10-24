7a501921e1f8ad70e95998bb506a9f555085b285
Benjamin Lavena <benja@zmaster.com.ar>
2024-10-24 10:24:03 -0300

    Added hints

--- aliases/pjt_aliases.zsh
+++ aliases/pjt_aliases.zsh
@@ -36,10 +36,13 @@ alias wakelazarus='ssh raspi400 wakelazarus'
 hints () {
   usage () {
     echo "Usage:"
-    echo "hints | hints a | hints all # Shows all hints"
     echo "hints -h | --help"
-    echo "hints fix_ownership"
+    echo "hints a | hints all # Shows all hints"
+    echo "hints db # Shows all DB hints"
     echo "hints create_database"
+    echo "hints dump_database"
+    echo "hints restore_database"
+    echo "hints fix_ownership"
   }
 
   create_database () {
@@ -47,7 +50,21 @@ hints () {
     echo "CREATE ROLE example WITH LOGIN
 VALID UNTIL 'infinity'
 NOINHERIT NOCREATEDB NOCREATEROLE NOREPLICATION NOSUPERUSER;
-CREATE DATABASE example WITH OWNER example;"
+CREATE DATABASE example WITH OWNER example;
+\c example
+GRANT CONNECT ON DATABASE example TO zbx_monitor;"
+    echo
+  }
+
+  dump_database () {
+    echo '### Dump database'
+    echo 'pg_dump --format=custom --no-acl --no-owner --no-privileges --file=database_name-$(date +%Y%m%d).dump database_name'
+    echo
+  }
+
+  restore_database () {
+    echo '### Restore database'
+    echo 'pg_restore -Fc -d database_name -xO file.dmp'
     echo
   }
 
@@ -57,13 +74,29 @@ CREATE DATABASE example WITH OWNER example;"
     echo
   }
 
+  show_all_hints () {
+    create_database
+    dump_database
+    restore_database
+    fix_ownership
+  }
+
+  show_all_db_hints () {
+    create_database
+    dump_database
+    restore_database
+    fix_ownership
+  }
+
   case "$1" in
-    a | all ) true;;
-    create_database | create_db ) create_database; return;;
+    a | all ) show_all_hints; return;;
+    db | show_all_dbs_hints ) show_all_db_hints; return;;
+    create_database | create_db | create) create_database; return;;
+    dump ) dump_database; return;;
+    restore ) restore_database; return;;
     fix_ownership ) fix_ownership; return;;
     -h | --help ) usage; return;;
   esac
   
-  fix_ownership
-  create_database
+  usage
 }
